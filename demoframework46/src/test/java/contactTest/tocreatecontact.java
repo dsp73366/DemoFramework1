2ec2845dea0703f3e6b40f5d50fad0ef6ced7c35
abhij <abhij@ABHIJIT>
2024-10-05 20:11:05 +0530

    jhh

--- demoframework46/src/test/java/contactTest/tocreatecontact.java
+++ demoframework46/src/test/java/contactTest/tocreatecontact.java
@@ -3,8 +3,7 @@ package contactTest;
 public class tocreatecontact {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+System.out.println("created successfully");
 	}
 
 }
---
+++ demoframework46/src/test/java/leadTest/tocreatelead.java
@@ -0,0 +1,9 @@
+package leadTest;
+
+public class tocreatelead {
+
+	public static void main(String[] args) {
+System.out.println("");
+	}
+
+}
